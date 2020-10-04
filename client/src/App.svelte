713cecfed0e0cba69a394b9e9c6763b9c8dd30cb
Alexandro Disla <alexandrodisla@hotmail.com>
2020-10-04 14:58:58 -0400

    signup form is working

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
   import Router from "svelte-spa-router";
   import Home from "./pages/Home.svelte";
+  import Dashboard from "./pages/Dashboard.svelte";
+  import Signup from "./pages/Signup.svelte";
 
   const routes = {
     "/": Home,
+    "/dashboard": Dashboard,
+    "/signup": Signup,
   };
 </script>
 

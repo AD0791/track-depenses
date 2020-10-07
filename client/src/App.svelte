935dd2b9af2e3c618dc9756783aa617a2bd369e8
Alexandro Disla <alexandrodisla@hotmail.com>
2020-10-07 14:38:18 -0400

    routes are protected from the client

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,17 +1,52 @@
 <script>
-  import Router from "svelte-spa-router";
+  import Router, { wrap, push } from "svelte-spa-router";
   import Home from "./pages/Home.svelte";
   import Dashboard from "./pages/Dashboard.svelte";
   import Signup from "./pages/Signup.svelte";
   import Navbar from "./components/Navbar.svelte";
   import Login from "./pages/Login.svelte";
+  import { onMount } from "svelte";
+  import axios from "axios";
+  import { user } from "./store";
+  import Loading from "./components/Loading.svelte";
+  let loading = true;
+  onMount(async () => {
+    const { data } = await axios.get("api/auth/user");
+    $user = data.user;
+    loading = false;
+  });
   const routes = {
-    "/": Home,
-    "/dashboard": Dashboard,
-    "/signup": Signup,
-    "/login": Login,
+    "/": wrap(Home, { reason: "authenticated" }, () => !$user),
+    "/dashboard": wrap(Dashboard, { reason: "unauthenticated" }, () => $user),
+    "/signup": wrap(Signup, { reason: "authenticated" }, () => !$user),
+    "/login": wrap(Login, { reason: "authenticated" }, () => !$user),
   };
+  function conditionsFailed(event) {
+    const { reason } = event.detail.userData;
+    switch (reason) {
+      case "unauthenticated":
+        return push("/login");
+      case "authenticated":
+        return push("/dashboard");
+    }
+  }
 </script>
 
-<Navbar />
-<Router {routes} />
+<style>
+  .loading-container {
+    max-width: 500px;
+    display: flex;
+    min-height: 100vh;
+    align-content: center;
+    margin: auto;
+  }
+</style>
+
+{#if loading}
+  <div class="loading-container">
+    <Loading />
+  </div>
+{:else}
+  <Navbar />
+  <Router {routes} on:conditionsFailed={conditionsFailed} />
+{/if}
